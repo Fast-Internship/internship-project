c6521b3c3eb5c973f72543599e27415df490868e
Davit Sahakyan <davit.sahakyan@finconstruct.com>
2020-10-08 10:54:22 +0400

    Add validation error messages

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,9 @@
+export abstract class Constants {
+  static readonly ERROR_MESAGES: {} = {
+    required: 'This Field is required',
+    email_type: 'Wrong type of email.',
+    password_minlength: 'Min length of password must be 8',
+    login_minlength: 'Min length of login must be 6',
+    without_whitespaces: "Field mustn't contain whitespace",
+  };
+}
