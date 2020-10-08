f3b1f0a2f41bc2d566878dd2e8595a73b1285833
Davit Sahakyan <davit.sahakyan@finconstruct.com>
2020-10-08 17:44:39 +0400

    Add car add button with navigation

--- src/app/components/car-list/car-list.component.ts
+++ src/app/components/car-list/car-list.component.ts
@@ -1,32 +1,41 @@
 import { Component, DoCheck, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Car } from 'src/app/core/models/car.model';
-import { CarService } from 'src/app/core/services/car-service'
+import { CarService } from 'src/app/core/services/car-service';
 
 @Component({
   selector: 'app-car-list',
   templateUrl: './car-list.component.html',
-  styleUrls: ['./car-list.component.css']
+  styleUrls: ['./car-list.component.css'],
 })
 export class CarListComponent implements OnInit, DoCheck {
   carsArray: Car[];
-  carTitles: Array<string>=["Brand","Class","Date","Horsepower","Model","Transmission"];
+  carTitles: Array<string> = [
+    'Brand',
+    'Class',
+    'Date',
+    'Horsepower',
+    'Model',
+    'Transmission',
+  ];
   slicedCars: Car[];
   pages: number;
 
-
-  constructor(private carService: CarService) { 
-  }
+  constructor(private carService: CarService, private router: Router) {}
 
   ngOnInit(): void {
-    this.carService.fetchCars()
-      .subscribe(carsArray => {
-        this.carsArray = carsArray;
-        localStorage.setItem('carsArray', JSON.stringify(this.carsArray))
-        this.pages = Math.ceil(carsArray.length / this.carService.rows);
-      });         
+    this.carService.fetchCars().subscribe((carsArray) => {
+      this.carsArray = carsArray;
+      localStorage.setItem('carsArray', JSON.stringify(this.carsArray));
+      this.pages = Math.ceil(carsArray.length / this.carService.rows);
+    });
   }
 
   ngDoCheck() {
     this.slicedCars = this.carService.sliceCars(this.carsArray);
+  }
+
+  goToAddCarPage() {
+    this.router.navigate(['add-car']);
   }
 }
